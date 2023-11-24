1c1e94e2d3cb2932520be677b37f708511c00f74
Kenechukwu Ubesie <ubesiekene@gmail.com>
2023-11-24 09:34:14 +0100

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { FaBars } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "./images/logo.png";
 
 import "./navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <nav className="navbar">
         <a href="/" className="logo">
           <img src={logo} alt="Logo" width={80} />
         </a>
-        <div className="hamburger">
-          <FaBars />
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={30} style={{ color: "#ffffff" }} />
+          ) : (
+            <FaBars size={30} style={{ color: "#ffffff" }} />
+          )}
         </div>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
             <a href="/">Home</a>
           </li>
