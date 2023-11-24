dfb97f4401772a830f62bfc6bc1c0804f592200f
Kenechukwu Ubesie <ubesiekene@gmail.com>
2023-11-24 13:00:09 +0100

    hero init

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="content">
+        <p>Call Us</p>
+        <p>1-800-123-4567</p>
+        <p>Because Money</p>
+        <p>Doesn't Come With Instructions</p>
+        <button className="button">Free Consultation</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
